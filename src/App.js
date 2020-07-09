82c8cb16202eddccaa7cefbc87b9b26c51d05648
sadok.mourad-mhiri <sadok.mourad-mhiri@proxym-it.com>
2020-07-09 10:47:53 +0100

    form component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
